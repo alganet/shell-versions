d0646d456e5b13710cf37bc5a587329c25077ffd
Alexandre Gaigalas <alganet+github@gmail.com>
2024-07-06 03:50:44 -0300

    Include Apple's zsh distribution
    This change adds zsh since version 84 (4.2.3) distributed from apple

--- variants/zsh.sh
+++ variants/zsh.sh
@@ -8,6 +8,8 @@ shvr_current_zsh ()
 	cat <<-@
 		zsh_5.9
 		zsh_5.8.1
+		zsh_appl-100.120.1
+		zsh_appl-100
 	@
 }
 
@@ -25,53 +27,76 @@ shvr_targets_zsh ()
 		zsh_5.1.1
 		zsh_5.0.8
 		zsh_4.2.7
+		zsh_appl-100.120.1
+		zsh_appl-100
+		zsh_appl-97
+		zsh_appl-94
+		zsh_appl-92
+		zsh_appl-90
+		zsh_appl-87
+		zsh_appl-84.120.1
+		zsh_appl-84.100.1
+		zsh_appl-84
 	@
 }
 
 shvr_build_zsh ()
 {
 	version="$1"
-	version_major="${version%%\.*}"
-
-	if test "$version" = "$version_major"
-	then return 1
-	fi
-	
-	version_minor="${version#$version_major\.}"
-	version_patch="${version_minor#*\.}"
-	
-	if test "$version_patch" = "$version_minor"
-	then version_patch="0"
-	else version_minor="${version_minor%\.*}"
-	fi
-	
+	fork_name="${1%%-*}"
+	fork_version="${1#*-}"
 	build_srcdir="${SHVR_DIR_SRC}/zsh/${version}"
 	mkdir -p "${build_srcdir}"
 
-	if 
-		test "$version_major" -gt 4 -a "${version_minor}" -gt 0 ||
-		test "$version_major" -gt 5
-	then
-		apt-get -y install \
-			wget gcc make autoconf libtinfo-dev xz-utils
-		wget -O "${build_srcdir}.tar.xz" \
-			"https://downloads.sourceforge.net/project/zsh/zsh/$version/zsh-$version.tar.xz"
-		tar --extract \
-			--file="${build_srcdir}.tar.xz" \
-			--strip-components=1 \
-			--directory="${build_srcdir}"
-	else
-		apt-get -y install \
-			wget gcc make autoconf libtinfo-dev
-		wget -O "${build_srcdir}.tar.gz" \
-			"https://downloads.sourceforge.net/project/zsh/zsh/$version/zsh-$version.tar.gz"
-		tar --extract \
-			--file="${build_srcdir}.tar.gz" \
-			--strip-components=1 \
-			--directory="${build_srcdir}"
-	fi
+	case "$fork_name" in
+		*'appl')
+			build_innerdir="zsh"
+			archiver="gz"
+			version_major=-1
+			version_url="https://github.com/apple-oss-distributions/zsh/archive/refs/tags/zsh-$fork_version.tar.gz"
 
-	cd "${build_srcdir}"
+			apt-get -y install \
+				wget gcc make autoconf libtinfo-dev
+			;;
+		*)
+			build_innerdir="."
+			version_major="${version%%\.*}"
+
+			if test "$version" = "$version_major"
+			then return 1
+			fi
+
+			version_minor="${version#$version_major\.}"
+			version_patch="${version_minor#*\.}"
+
+			if test "$version_patch" = "$version_minor"
+			then version_patch="0"
+			else version_minor="${version_minor%\.*}"
+			fi
+
+			if
+				test "$version_major" -gt 4 -a "${version_minor}" -gt 0 ||
+				test "$version_major" -gt 5
+			then
+				archiver="xz"
+				apt-get -y install \
+					wget gcc make autoconf libtinfo-dev xz-utils
+			else
+				archiver="gz"
+				apt-get -y install \
+					wget gcc make autoconf libtinfo-dev
+			fi
+
+			version_url="https://downloads.sourceforge.net/project/zsh/zsh/$version/zsh-$version.tar.$archiver"
+			;;
+	esac
+	wget -O "${build_srcdir}.tar.$archiver" "$version_url"
+	tar --extract \
+		--file="${build_srcdir}.tar.$archiver" \
+		--strip-components=1 \
+		--directory="${build_srcdir}"
+
+	cd "${build_srcdir}/${build_innerdir}"
 
 	./Util/preconfig
 	./configure \
