bb5c042b7c9c347c456a5e61da875137c6c19c8d
Alexandre Gomes Gaigalas <alganet@gmail.com>
2025-06-25 21:52:41 -0300

    Update osh to 0.33
    osh has changed the domain and build steps, our script has changed
    to reflect those changes.
    
    osh moves fast, so we decided to remove the older versions, keeping
    just a handful of the previous incarnation, thus reducing the
    amount of osh versions available.

--- variants/osh.sh
+++ variants/osh.sh
@@ -6,43 +6,61 @@
 shvr_current_osh ()
 {
 	cat <<-@
-		osh_0.24.0
-		osh_0.23.0
+		osh_0.33.0
+		osh_0.32.0
 	@
 }
 
 shvr_targets_osh ()
 {
 	cat <<-@
+		osh_0.33.0
+		osh_0.32.0
+		osh_0.31.0
+		osh_0.30.0
+		osh_0.29.0
+		osh_0.28.0
+		osh_0.27.0
+		osh_0.26.0
+		osh_0.25.0
 		osh_0.24.0
 		osh_0.23.0
 		osh_0.22.0
-		osh_0.21.0
-		osh_0.20.0
-		osh_0.19.0
-		osh_0.18.0
-		osh_0.17.0
-		osh_0.16.0
-		osh_0.15.0
-		osh_0.14.2
-		osh_0.13.1
-		osh_0.12.9
-		osh_0.11.0
-		osh_0.10.1
-		osh_0.9.9
 	@
 }
 
 shvr_build_osh ()
 {
 	version="$1"
+	version_major="${version%%\.*}"
+
+	if test "$version" = "$version_major"
+	then return 1
+	fi
+
+	version_minor="${version#$version_major\.}"
+	version_patch="${version_minor#*[.-]}"
+
+	if test "$version_patch" = "$version_minor"
+	then return 1
+	else version_minor="${version_minor%\.*}"
+	fi
+
 	build_srcdir="${SHVR_DIR_SRC}/osh/${version}"
 	mkdir -p "${build_srcdir}"
 
-	apt-get -y install \
-		wget gcc make
-	wget -O "${build_srcdir}.tar.gz" \
-		"https://www.oilshell.org/download/oil-${version}.tar.gz"
+	if test "$version_major" = 0 && test "$version_minor" -lt 25
+	then
+		apt-get -y install \
+			wget gcc make
+		wget -O "${build_srcdir}.tar.gz" \
+			"https://www.oilshell.org/download/oil-${version}.tar.gz"
+	else
+		apt-get -y install \
+			wget gcc g++ make
+		wget -O "${build_srcdir}.tar.gz" \
+			"https://oils.pub/download/oils-for-unix-${version}.tar.gz"
+	fi
 
 	tar --extract \
 		--file="${build_srcdir}.tar.gz" \
@@ -55,9 +73,17 @@ shvr_build_osh ()
 		--without-readline \
 		--prefix="${SHVR_DIR_OUT}/osh_$version"
 
-	make -j "$(nproc)"
-	mkdir -p "${SHVR_DIR_OUT}/osh_${version}/bin"
-	cp "_bin/oil.ovm" "${SHVR_DIR_OUT}/osh_$version/bin/osh"
-	
+	if test "$version_major" = 0 && test "$version_minor" -lt 25
+	then
+		make -j "$(nproc)"
+		mkdir -p "${SHVR_DIR_OUT}/osh_${version}/bin"
+		cp "_bin/oil.ovm" "${SHVR_DIR_OUT}/osh_$version/bin/osh"
+	else
+		_build/oils.sh
+		mkdir -p "${SHVR_DIR_OUT}/osh_${version}/bin"
+		cp "_bin/cxx-opt-sh/oils-for-unix" "${SHVR_DIR_OUT}/osh_$version/bin/osh"
+	fi
+
+
 	"${SHVR_DIR_OUT}/osh_${version}/bin/osh" -c "echo osh version $version"
 }
