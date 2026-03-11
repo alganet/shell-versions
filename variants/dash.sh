4899cc2d1c408048fc189838f114580d7e3acbda
Alexandre Gomes Gaigalas <alganet@gmail.com>
2026-03-11 18:30:33 -0300

    Build dash statically with musl

--- variants/dash.sh
+++ variants/dash.sh
@@ -3,6 +3,13 @@
 # SPDX-FileCopyrightText: 2025 Alexandre Gomes Gaigalas <alganet@gmail.com>
 # SPDX-License-Identifier: ISC
 
+. "${SHVR_DIR_SELF}/common/musl-cross-make.sh"
+
+shvr_static_dash ()
+{
+	return 0
+}
+
 shvr_current_dash ()
 {
 	cat <<-@
@@ -54,9 +61,6 @@ shvr_build_dash ()
 
 	cd "${build_srcdir}"
 
-	# TODO remove this dependency
-	build_arch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)"
-
 	if test -f ./autogen.sh
 	then
 		./autogen.sh
@@ -67,31 +71,27 @@ shvr_build_dash ()
 		autoconf
 	fi
 
-	# Build with reproducible flags
-	# Use fixed source date epoch and disable compiler timestamp features
+	# Static musl build with reproducible flags
 	export SOURCE_DATE_EPOCH=1
 	export TZ=UTC
+	export CC="$(shvr_musl_cc) -static"
+	export AR="$(shvr_musl_ar)"
+	export RANLIB="$(shvr_musl_ranlib)"
 	export CFLAGS="-frandom-seed=1"
 	export LDFLAGS="-Wl,--build-id=none"
-	export RANLIB="ranlib -D"
-	export AR="ar -D"
 
 	./configure \
-		--build="$build_arch" \
+		--host=x86_64-linux-musl \
 		--prefix="${SHVR_DIR_OUT}/dash_$version"
 
-	# Single-threaded build for deterministic ordering
 	make
 
-	unset SOURCE_DATE_EPOCH TZ CFLAGS LDFLAGS RANLIB AR
+	unset SOURCE_DATE_EPOCH TZ CC AR RANLIB CFLAGS LDFLAGS
 
 	mkdir -p "${SHVR_DIR_OUT}/dash_${version}/bin"
 	cp "src/dash" "${SHVR_DIR_OUT}/dash_$version/bin/dash"
 
-	# Strip binary to ensure reproducible output
-	strip --strip-all "${SHVR_DIR_OUT}/dash_${version}/bin/dash"
-
-	# Ensure consistent permissions and timestamps
+	"$(shvr_musl_strip)" --strip-all "${SHVR_DIR_OUT}/dash_${version}/bin/dash"
 	touch -d "@1" "${SHVR_DIR_OUT}/dash_${version}/bin/dash"
 	chmod 755 "${SHVR_DIR_OUT}/dash_${version}/bin/dash"
 
@@ -102,5 +102,5 @@ shvr_deps_dash ()
 {
 	shvr_versioninfo_dash "$1"
 	apt-get -y install \
-		curl gcc automake autoconf dpkg-dev binutils
+		curl automake autoconf
 }
